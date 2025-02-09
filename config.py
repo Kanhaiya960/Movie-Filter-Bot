94f76d494f3def3b82195c1df7ee604e4d51599a
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2025-02-10 01:08:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+API_ID = "your_api_id"  # From my.telegram.org
+API_HASH = "your_api_hash"  # From my.telegram.org
+BOT_TOKEN = "your_bot_token"  # From BotFather
+MONGO_URI = "your_mongodb_uri"  # MongoDB connection string
+ADMIN_ID = 123456789  # Your Telegram user ID
